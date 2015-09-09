b5bf3aa9c1a0b0e9369a3fddb47d959b1d04c1fd
Edwin Phillips <edwin.phillips@catalyst-eu.net>
2015-09-09 11:25:01 +0100

    Replace 'Single/Multi Answers' select menu with hidden form element

--- edit_turprove_form.php
+++ edit_turprove_form.php
@@ -49,9 +49,8 @@ class qtype_turprove_edit_form extends question_edit_form {
             get_string('answersingleno', 'qtype_turprove'),
             get_string('answersingleyes', 'qtype_turprove'),
         );
-        $mform->addElement('select', 'single',
-                get_string('answerhowmany', 'qtype_turprove'), $menu);
-        $mform->setDefault('single', 0);
+
+        $mform->addElement('hidden', 'single', '0');
 
         // 'Image to display' filemanager
         $mform->addElement('filemanager', 'questionimage', 'Image to display', null,
